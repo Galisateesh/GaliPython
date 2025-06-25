7194221374eb790ef95f417896207fb7d655f646
Gali Sateesh <galisateesh542@gmail.com>
2025-06-25 16:04:18 +0530

    string functions
    all string functions

---
+++ string_functions.py
@@ -0,0 +1,26 @@
+s="python developer"
+print(s.capitalize())#Python developer
+print(s.title())#Python Developer
+print(s.islower())#True
+print(s.isupper())#False
+print(s.lower())#python developer
+print(s.upper())#PYTHON DEVELOPER
+print(len(s))#16
+print(s.count('e'))#3
+print(s.find('o'))#4
+print(s.find('o',7))#12(second occurance above 4)
+print(s.split(' '))#['python', 'developer']
+t="!!!!!!!!!python developer!!!!!!!!"
+print(t.lstrip('!'))#python developer!!!!!!!!
+print(t.rstrip('!'))#!!!!!!!!!python developer
+print(t.strip('!'))#python developer
+print(t.swapcase())#!!!!!!!!!PYTHON DEVELOPER!!!!!!!!
+print(''.join(reversed(t)))#!!!!!!!!repoleved nohtyp!!!!!!!!!
+print(t.replace('e','A'))#!!!!!!!!!python dAvAlopAr!!!!!!!!
+
+
+
+
+
+
+
