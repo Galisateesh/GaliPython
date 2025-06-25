eaab395a15957e42ef787f4c434f558607d0cb15
Gali Sateesh <galisateesh542@gmail.com>
2025-06-25 07:41:53 +0530

    algebraic
    Finding maximum number which is entered by the user

---
+++ maximum_of_numbers.py
@@ -0,0 +1,18 @@
+class gali:
+    def fun(self,arr):
+        ma=0
+        for i in arr:
+            if i>ma:
+                ma=i
+        return ma
+n=[]
+k=int(input("enter how many numbers"))
+print("enter those numbers")
+for i in range(k):
+    i=int(input())
+    n.append(i)
+c=gali()
+o=c.fun(n)
+print("max value",o)
+
+        
