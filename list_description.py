4c7a6647bbfdb302ab7332c8531bc2c447df38c5
Gali Sateesh <galisateesh542@gmail.com>
2025-06-27 09:03:27 +0530

    list
    list description

---
+++ list_description.py
@@ -0,0 +1,28 @@
+1.A list is a collection of elements. These elements may be homogeneous or heterogeneous.
+
+2.A list is a value that contains multiple values in an ordered sequence. The term list value refers to the
+   list itself (which is a value that can be stored in a variable or passed to a function like any other value)
+   
+3.Just as string values are typed with quote characters to mark where the string begins and ends, a list 
+   begins with an opening square bracket and ends with a closing square bracket, [].
+   
+4.Values inside the list are also called items. Items are separated with commas (that is, they are comma-
+   delimited).
+
+5.A list also allows duplicate elements.
+
+6.Insertion order is preserved in list.
+
+7.List elements are separated by commas and enclosed within square brackets ([]).
+
+8.Every element in the list has its own unique index number.
+
+9.List supports both forward indexing and backward indexing, forward index starts from 0 and backward 
+  index starts from -1.
+  
+10.We access either specific element by using indexing or set of elements by using slicing from the List.
+
+11.We can create list in different ways. Like by using list() function, by using square brackets “[]” and also 
+   by using range() function.
+   
+12.List objects are mutable.
