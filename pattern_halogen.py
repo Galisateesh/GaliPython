1941a3569520eb824f360ab004fa6bc836459a88
Gali Sateesh <galisateesh542@gmail.com>
2025-06-27 11:28:41 +0530

    pattern_halogen.py
    Update

--- pattern_halogen.py
+++ pattern_halogen.py
@@ -1,7 +1,7 @@
 n=5
 for i in range(n):
     for j in range(n):
-        if i>0 and j>0 and i<4 and j<4:
+        if i>0 and j>0 and i<n-1 and j<n-1:
             print(' ',end=' ')
         else:
             print('*',end=' ')
